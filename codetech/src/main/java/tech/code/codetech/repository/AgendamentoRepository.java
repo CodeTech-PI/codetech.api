e960f626b990d5f996ac78d8c54e76916ac3872b
hosanaflores <hosana.flores@sptech.school>
2024-08-31 22:58:57 -0300

    Refactor/Excluindo classes product da branch
    Exclusão das classes de produto da branch feature/crud-agendamento

--- codetech/src/main/java/tech/code/codetech/repository/AgendamentoRepository.java
+++ codetech/src/main/java/tech/code/codetech/repository/AgendamentoRepository.java
@@ -1,9 +1,7 @@
 package tech.code.codetech.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import tech.code.codetech.model.Agendamento;
 
-@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
 }
--- codetech/src/main/java/tech/code/codetech/service/AgendamentoService.java
+++ codetech/src/main/java/tech/code/codetech/service/AgendamentoService.java
@@ -4,9 +4,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.code.codetech.model.Agendamento;
-import tech.code.codetech.model.Product;
 import tech.code.codetech.repository.AgendamentoRepository;
-import tech.code.codetech.repository.ProductRepository;
 
 import java.util.List;
 
