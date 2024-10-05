0eab209f804b82c2e5b1f8c0f2dd7804f864f564
hosanaflores <hosana.flores@sptech.school>
2024-10-05 18:06:49 -0300

    Feature/Dynamic Finders, criação para listar produtos por id de agendamento

--- codetech/src/main/java/tech/code/codetech/repository/ListaProdutoRepository.java
+++ codetech/src/main/java/tech/code/codetech/repository/ListaProdutoRepository.java
@@ -3,5 +3,9 @@ package tech.code.codetech.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.code.codetech.model.ListaProduto;
 
+import java.util.List;
+
 public interface ListaProdutoRepository extends JpaRepository<ListaProduto, Integer> {
+
+    List<ListaProduto> findByAgendamentoId (Integer id);
 }
