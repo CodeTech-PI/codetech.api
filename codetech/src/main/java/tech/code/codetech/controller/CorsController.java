f67ffff7479521eba5f52ff3e6ede302a2887b6e
gabrieloliveira0505 <gabriel.oliveira165@sptech.school>
2024-10-13 18:02:59 -0300

    adicionando cors no backend

---
+++ codetech/src/main/java/tech/code/codetech/controller/CorsController.java
@@ -0,0 +1,19 @@
+package tech.code.codetech.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsController implements WebMvcConfigurer  {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas as rotas
+                .allowedOrigins("http://localhost:3000") // Permitir requisições apenas dessa origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*"); // Permitir todos os cabeçalhos
+    }
+}
+
