0b07b922e708df5ddcfd4490e279f8b8a8bd1708
Samarah Silva <samarah.silva@jazztech.corp>
2024-10-16 09:55:25 -0300

    Arumando gerar token

--- codetech/src/main/java/tech/code/codetech/api/configuration/security/jwt/GerenciadorTokenJwt.java
+++ codetech/src/main/java/tech/code/codetech/api/configuration/security/jwt/GerenciadorTokenJwt.java
@@ -2,6 +2,7 @@ package tech.code.codetech.api.configuration.security.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
