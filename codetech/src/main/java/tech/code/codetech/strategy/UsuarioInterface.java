81627b501ffe24303cf82044f16df251b60e5169
hosanaflores <hosana.flores@sptech.school>
2024-10-26 19:58:27 -0300

    Refactor/Update dos usuarios lombardi Interface
    Havia public na interface e era desncessário os manter, por isso foram
    retirados.

--- codetech/src/main/java/tech/code/codetech/strategy/UsuarioInterface.java
+++ codetech/src/main/java/tech/code/codetech/strategy/UsuarioInterface.java
@@ -5,14 +5,13 @@ import tech.code.codetech.model.Usuario;
 import java.util.List;
 
 public interface UsuarioInterface {
-    public Usuario save(Usuario usuario);
+    Usuario save(Usuario usuario);
 
-    public List<Usuario> findAll();
+    List<Usuario> findAll();
 
-    public Usuario findById(Integer id);
+    Usuario findById(Integer id);
 
-    public Usuario update(Integer id, Usuario usuario);
+    Usuario update(Integer id, Usuario usuario);
 
-    public boolean delete(Integer id);
-
+    boolean delete(Integer id);
 }
