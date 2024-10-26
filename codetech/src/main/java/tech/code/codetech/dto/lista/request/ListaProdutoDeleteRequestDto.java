af51e9e805e19aeedae77edde512715c7e36fcf3
hosanaflores <hosana.flores@sptech.school>
2024-10-26 18:06:28 -0300

    Feature/Delete da lista de produtos (deleteAll)

---
+++ codetech/src/main/java/tech/code/codetech/dto/lista/request/ListaProdutoDeleteRequestDto.java
@@ -0,0 +1,18 @@
+package tech.code.codetech.dto.lista.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListaProdutoDeleteRequestDto {
+
+    private List<Integer> produtosIds;
+
+}
