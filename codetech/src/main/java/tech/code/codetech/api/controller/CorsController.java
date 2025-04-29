62af969ddb58e9dad34fad9dfc8ff1d000889405
GabrielaSeverino <gabriela.severino@sptech.school>
2025-04-29 15:39:31 -0300

    imagemBE

--- codetech/src/main/java/tech/code/codetech/api/controller/CorsController.java
+++ codetech/src/main/java/tech/code/codetech/api/controller/CorsController.java
@@ -12,6 +12,10 @@ public class CorsController implements WebMvcConfigurer  {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas as rotas
                 .allowedOrigins("http://localhost:3000") // Permitir requisições apenas dessa origem
+                .allowedOrigins("http://18.215.140.118")
+                .allowedOriginPatterns("http://18.215.140.118/*")
+                .allowedOriginPatterns("http://10.0.0.*")
+                .allowedOriginPatterns("http://10.0.1.*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*"); // Permitir todos os cabeçalhos
     }
