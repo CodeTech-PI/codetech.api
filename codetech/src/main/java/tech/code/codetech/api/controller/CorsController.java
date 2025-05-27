58db178fa057faecf09ca530b81e8ee990211b41
GabrielaSeverino <gabriela.severino@sptech.school>
2025-05-27 19:15:12 -0300

    novo ip elastico no cors

--- codetech/src/main/java/tech/code/codetech/api/controller/CorsController.java
+++ codetech/src/main/java/tech/code/codetech/api/controller/CorsController.java
@@ -11,10 +11,15 @@ public class CorsController implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas as rotas
-                .allowedOrigins("http://localhost:3000") // Permitir requisições apenas dessa origem
-                .allowedOrigins("http://3.219.248.230:3000")
-                .allowedOriginPatterns("http://3.219.248.230:3000/*")
-                .allowedOriginPatterns("http://3.219.248.230/*")
+                .allowedOrigins("http://localhost:3000") // Permitir requisições apenas dessa origem (desenvolvimento)
+                .allowedOrigins("http://44.217.25.5")    // Permitir requisições do IP elástico da EC2 pública
+                // Remova ou corrija os allowedOriginPatterns incorretos:
+                // .allowedOriginPatterns("44.217.25.5:80/*") // INCORRETO
+                // .allowedOriginPatterns("http://44.217.25.5/*") // NÃO IDEAL PARA IP ESPECÍFICO
+                //.allowedOrigins("http://localhost:3000") // Permitir requisições apenas dessa origem
+                //.allowedOrigins("http://44.217.25.5:80")
+                //.allowedOriginPatterns("44.217.25.5:80/*")
+                //.allowedOriginPatterns("http://44.217.25.5/*")
                 .allowedOriginPatterns("http://10.0.0.*")
                 .allowedOriginPatterns("http://10.0.1.*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
